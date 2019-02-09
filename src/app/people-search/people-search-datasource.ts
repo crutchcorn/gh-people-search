ade09efbcc8c0010aa02a2f8020eee7786ca1627
Crutchcorn <crutchcorn@gmail.com>
2019-02-09 14:17:53 -0800

    Got pagination logic hopefully finalized

--- src/app/people-search/people-search-datasource.ts
+++ src/app/people-search/people-search-datasource.ts
@@ -24,29 +24,28 @@ export class PeopleSearchDataSource extends DataSource<GithubUser> {
     // Set the paginator's initial length to 0, as we don't want default values
     this.paginator.length = 0;
 
-    // This ensures that whenever we expect to update the UI with new data (not necessarily refetch data)
+    // This ensures that whenever we expect to update the UI with new data (not necessarily re-fetch data)
     return merge(
       this.searchSubj,
       this.paginator.page,
       this.sort.sortChange
     ).pipe(switchMap(eventVal => {
-      // This should always be true with our config but is added for expandandability
+      // This should always be true with our config but is added for expandability
       const canHybridPage = (100 % this.paginator.pageSize) === 0;
       // Method to be used to return the valid dataset
-      const getReturnArr = () => {
-        if (canHybridPage) {
-          const leftOver = (this.paginator.pageSize * this.paginator.pageIndex) % 100;
-          return this.currentServerData.slice(leftOver, leftOver + this.paginator.pageSize);
-        } else {
-          return this.currentServerData;
-        }
+      const returnSlicedData = () => {
+        const leftOver = (this.paginator.pageSize * this.paginator.pageIndex) % 100;
+        return this.currentServerData.slice(leftOver, leftOver + this.paginator.pageSize);
       };
 
+      const getSafeReturnArr = () => canHybridPage ? returnSlicedData() : this.currentServerData;
+
       // When searchSubj changed, we need to reset to start and regrab data
+      // TODO: Break this out into it's own function - the logic is getting hard to track and it's time to break it out into a method
       if (typeof eventVal === 'string') {
         // Early return if data has not changed
         if (eventVal === this.searchSubj.value) {
-          return observableOf(getReturnArr());
+          return observableOf(getSafeReturnArr());
         }
         return this.gitHubService.searchUsers(this.searchSubj.value, 0, {
           // If it can handle hybrid page, it will be `false` which is removed from server call by object helper function
@@ -65,20 +64,53 @@ export class PeopleSearchDataSource extends DataSource<GithubUser> {
           }));
       }
 
-      // Page was changed in some way
+      // Page was changed in some way - use function to preserve type data
+      // TODO: Break this out into it's own function - the logic is getting hard to track and it's time to break it out into a method
       const isEventPageEvent = (eventInfo: any): eventInfo is PageEvent => !!(eventInfo as PageEvent).pageIndex;
       if (isEventPageEvent(eventVal)) {
+        if (!canHybridPage) {
+          return this.gitHubService.searchUsers(this.searchSubj.value, eventVal.pageIndex, {
+            per_page: this.paginator.pageSize,
+            order: this.sort.direction
+          }).pipe(map(({total_count, items}) => {
+            this.paginator.length = total_count;
+            this.currentServerData = items;
+            this.currentServerPage = eventVal.pageIndex;
+            return items;
+          }));
+        }
+        // This includes the current page of data
+        const numberOfItemsViewedClient = eventVal.pageSize * (eventVal.pageIndex + 1);
+        // Might be able to do some clever math (by using modulo and comparison) to remove the current server page requirement
+        // But we'd need to be able to compare the pageIndex against the previous one to check that a user hasn't switched pages/etc
+        const serverItemRange = this.currentServerPage * 100;
+        // Make sure the serverItemRange doesn't go out-of-bounds above or below
+        if ((serverItemRange > numberOfItemsViewedClient) && ((serverItemRange - (numberOfItemsViewedClient)) < 100)) {
+          // return slice as we have the data already grabbed
+          return observableOf(returnSlicedData());
+        }
 
+        // fetch new page as we didn't have the data
+        // We could probably currServPage++, but that would be assuming the state we're moving in and breaks going backward/etc
+        this.currentServerPage = Math.floor(numberOfItemsViewedClient / 100);
+        return this.gitHubService.searchUsers(this.searchSubj.value, this.currentServerPage, {
+          order: this.sort.direction
+        }).pipe(map(({total_count, items}) => {
+          this.paginator.length = total_count;
+          this.currentServerData = items;
+          return returnSlicedData();
+        }));
       }
 
-      // Sort was changed
+
+      // Sort was changed - use function to preserve type data
       const isEventSort = (eventInfo: any): eventInfo is Sort => !!(eventInfo as Sort).direction;
       if (isEventSort(eventVal)) {
 
       }
 
       // Otherwise safe return in case of edgecase
-      return observableOf(getReturnArr());
+      return observableOf(getSafeReturnArr());
     }));
   }
 
