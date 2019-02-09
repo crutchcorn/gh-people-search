88187241e2a822366916ac63f539b234540ef85c
Crutchcorn <crutchcorn@gmail.com>
2019-02-09 02:12:33 -0800

    Fix some logic with hybrid pagination

--- src/app/people-search/people-search-datasource.ts
+++ src/app/people-search/people-search-datasource.ts
@@ -30,28 +30,45 @@ export class PeopleSearchDataSource extends DataSource<GithubUser> {
       this.paginator.page,
       this.sort.sortChange
     ).pipe(switchMap(eventVal => {
+      // This should always be true with our config but is added for expandandability
+      const canHybridPage = (100 % this.paginator.pageSize) === 0;
+      // Method to be used to return the valid dataset
+      const getReturnArr = () => {
+        if (canHybridPage) {
+          const leftOver = (this.paginator.pageSize * this.paginator.pageIndex) % 100;
+          return this.currentServerData.slice(leftOver, leftOver + this.paginator.pageSize);
+        } else {
+          return this.currentServerData;
+        }
+      };
+
       // When searchSubj changed, we need to reset to start and regrab data
       if (typeof eventVal === 'string') {
         // Early return if data has not changed
         if (eventVal === this.searchSubj.value) {
-          return observableOf(this.currentServerData.slice(this.paginator.pageIndex, this.paginator.pageSize));
+          return observableOf(getReturnArr());
         }
-        return this.gitHubService.searchUsers(this.searchSubj.value, 0, {order: this.sort.direction})
+        return this.gitHubService.searchUsers(this.searchSubj.value, 0, {
+          // If it can handle hybrid page, it will be `false` which is removed from server call by object helper function
+          per_page: (!canHybridPage) && this.paginator.pageSize,
+          order: this.sort.direction
+        })
           .pipe(map(({total_count, items}) => {
             this.paginator.length = total_count;
             this.paginator.firstPage();
             this.currentServerData = items;
             this.currentServerPage = 0;
             // Return only the actual amount of items we need to. We're going to be fetching more from the server to limit the number of
+            // We can only do this if `(100 % this.paginator.pageSize) === 0` otherwise things don't work out
             // API calls (GH's v3 API limits pretty heavily so this should help a bit with that)
-            return items.slice(0, this.paginator.pageSize);
+            return canHybridPage ? items.slice(0, this.paginator.pageSize) : items;
           }));
       }
 
       // Page was changed in some way
       const isEventPageEvent = (eventInfo: any): eventInfo is PageEvent => !!(eventInfo as PageEvent).pageIndex;
       if (isEventPageEvent(eventVal)) {
-        
+
       }
 
       // Sort was changed
@@ -61,7 +78,7 @@ export class PeopleSearchDataSource extends DataSource<GithubUser> {
       }
 
       // Otherwise safe return in case of edgecase
-      return observableOf(this.currentServerData.slice(this.paginator.pageIndex, this.paginator.pageSize));
+      return observableOf(getReturnArr());
     }));
   }
 
